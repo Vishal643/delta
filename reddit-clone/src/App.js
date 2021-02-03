2e38e6d5013d54546aa23985e08614c6d8b4caf8
Siddharth308 <prabhu.sid97@gmail.com>
2021-02-03 14:49:49 +0530

    left-part

--- reddit-clone/src/App.js
+++ reddit-clone/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import {Sidebar} from './Components/Sider'
 import RightCard from './Components/RightCards';
 const App = () => {
 	return (
 		<div>
+			<Sidebar/>
 			<RightCard />
 		</div>
 	);
