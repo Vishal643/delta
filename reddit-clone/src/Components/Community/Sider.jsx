47ba9633131cef524a830a9a643d81eaaf2d5527
Vishal643 <vishalkumar199812@gmail.com>
2021-02-05 14:59:19 +0530

    made some changes

--- reddit-clone/src/Components/Community/Sider.jsx
+++ reddit-clone/src/Components/Community/Sider.jsx
@@ -13,7 +13,6 @@ const { Header, Content, Sider } = Layout;
 export const Sidebar = () => {
 	// const [isLoading, setIsLoading] = React.useState(false);
 	const [showResults, setShowResults] = React.useState(true);
-
 	const [data, setData] = React.useState([]);
 	const hide = () => {
 		setShowResults(!showResults);
@@ -28,17 +27,73 @@ export const Sidebar = () => {
 				console.log(err);
 			});
 	};
-	// React.useEffect(() => {
-	//     console.log(showResults)
+	const getNearYou = () => {
+		getAllCommunities()
+			.then((res) => {
+				console.log(res);
+				setData([]);
+				setData(res.data[0].Near_You);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const getSportsData = () => {
+		getAllCommunities()
+			.then((res) => {
+				console.log(res.data);
+				setData([]);
+				setData(res.data[0].Sports);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const getGamingData = () => {
+		getAllCommunities()
+			.then((res) => {
+				console.log(res.data);
+				setData([]);
+				setData(res.data[0].Gaming);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const getNewsData = () => {
+		getAllCommunities()
+			.then((res) => {
+				console.log(res.data);
+				setData([]);
+				setData(res.data[0].News);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+	const getMemesData = () => {
+		getAllCommunities()
+			.then((res) => {
+				console.log(res.data);
+				setData([]);
+				setData(res.data[0].Meme);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-	//   },[showResults]);
 	return (
 		<>
 			<Layout>
 				<Header style={{ backgroundColor: 'white', height: '100px' }}>
-					<h3 style={{ marginLeft: '150px' }}>
+					<h3 style={{ marginLeft: '150px', marginBottom: 0 }}>
 						Today's Top Growing Communities
 					</h3>
+					<p style={{ marginLeft: '150px' }}>
+						Browse Reddit's top growing communities. Find the top communities in
+						your favorite category.
+					</p>
 				</Header>
 				<Content style={{ padding: '0 50px', marginLeft: '150px' }}>
 					<Layout
@@ -60,11 +115,21 @@ export const Sidebar = () => {
 								<Menu.Item key='2' onClick={getData}>
 									All Communities
 								</Menu.Item>
-								<Menu.Item key='3'>Near You</Menu.Item>
-								<Menu.Item key='4'>Sports</Menu.Item>
-								<Menu.Item key='5'>Gaming</Menu.Item>
-								<Menu.Item key='6'>Aww</Menu.Item>
-								<Menu.Item key='7'>Memes</Menu.Item>
+								<Menu.Item key='3' onClick={getNearYou}>
+									Near You
+								</Menu.Item>
+								<Menu.Item key='4' onClick={getSportsData}>
+									Sports
+								</Menu.Item>
+								<Menu.Item key='5' onClick={getGamingData}>
+									Gaming
+								</Menu.Item>
+								<Menu.Item key='6' onClick={getNewsData}>
+									News
+								</Menu.Item>
+								<Menu.Item key='7' onClick={getMemesData}>
+									Memes
+								</Menu.Item>
 								{/* </SubMenu> */}
 								<SubMenu
 									className={showResults ? styless.btn : styless.btn}
@@ -73,34 +138,80 @@ export const Sidebar = () => {
 									key='sub2'
 									title={showResults ? 'Showmore' : 'Showless'}
 								>
-									<Menu.Item key='5'>option5</Menu.Item>
-									<Menu.Item key='6'>option6</Menu.Item>
-									<Menu.Item key='7'>option7</Menu.Item>
-									<Menu.Item key='8'>option8</Menu.Item>
+									<Menu.Item key='5'>Music</Menu.Item>
+									<Menu.Item key='6'>Travel</Menu.Item>
+									<Menu.Item key='7'>Tech</Menu.Item>
+									<Menu.Item key='8'>Memes</Menu.Item>
 								</SubMenu>
 							</Menu>
 						</Sider>
 						<Content style={{ padding: '0 24px', minHeight: 280 }}>
 							<div className={styless.cards}>
-								{data?.map((item, i) => (
-									<div>
-										{item.id}
-										<i
-											className='fa fa-caret-up'
-											style={{ fontsize: '48px', color: 'green' }}
-										></i>
-										<img src={item.img} alt='photos' />
-										{item.title}{' '}
+								{data.length !== 0 ? (
+									<div
+										style={{
+											height: '35px',
+											paddingTop: 0,
+											backgroundColor: 'rgb(231, 231, 231)',
+										}}
+									>
+										Today's Top Growing Communities
 									</div>
+								) : null}
+								{data?.map((item, i) => (
+									<>
+										<div>
+											{item.id}
+											<i
+												className='fa fa-caret-up'
+												style={{ fontsize: '48px', color: 'green' }}
+											></i>
+											<div className={styless.details}>
+												<img
+													className={styless.icons}
+													src={item.img}
+													alt='photos'
+												/>
+												<span>{item.title}</span>
+												<div style={{ display: 'flex' }}>
+													<div className={styless.members}>
+														<p>{item.members}</p>
+														<br />
+														Members
+													</div>
+
+													<div className={styless.online}>
+														<p>{item.online}</p>
+														<br />
+														Online
+													</div>
+												</div>
+												<br />
+												<div className={styless.tagline}>
+													<p>{item.tagline}</p>
+												</div>
+												<button
+													// onClick={() => }
+													className={styless.see_all_btn}
+												>
+													View Community
+												</button>
+											</div>
+											<img
+												className={styless.icons}
+												src={item.img}
+												alt='photos'
+											/>
+											<span>{item.title}</span>{' '}
+										</div>
+									</>
 								))}
 							</div>
 						</Content>
-
 						<RightCards />
 					</Layout>
 				</Content>
 			</Layout>
-			,
 		</>
 	);
 };
--- reddit-clone/src/Components/Community/utils.js
+++ reddit-clone/src/Components/Community/utils.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 function getAllCommunities() {
-	return axios.get('https://reddit-mock-server.herokuapp.com/reddit');
+	return axios.get('https://fast-lake-64955.herokuapp.com/reddit');
 }
 
-function getNearYou() {}
-
-function getSports() {}
-
-function getGaming() {}
-export { getAllCommunities, getNearYou, getSports, getGaming };
+export { getAllCommunities };
