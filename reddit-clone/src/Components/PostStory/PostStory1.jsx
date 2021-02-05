97366b3ed4145fa10f896ff1d4c37c6309b02428
udit22022000 <ping2udit@gmail.com>
2021-02-05 14:15:47 +0530

    Post a Story with an image

--- reddit-clone/src/Components/PostStory/PostStory1.jsx
+++ reddit-clone/src/Components/PostStory/PostStory1.jsx
@@ -2,11 +2,8 @@ import React from "react";
 import styles from "./PostStory1.module.css";
 import axios from "axios";
 import { v4 as uuid } from "uuid";
-import { ImgVidForm } from "./ImgVidForm";
 import MessageIcon from "@material-ui/icons/Message";
 import ImageIcon from "@material-ui/icons/Image";
-import LinkIcon from "@material-ui/icons/Link";
-import PollIcon from "@material-ui/icons/Poll";
 
 import { PostStory2 } from "./PostStory2";
 
@@ -62,7 +59,7 @@ const PostStory1 = () => {
       id: post_id,
       title: title1,
       upvotes: 0,
-      img_src: null,
+      image_src: null,
       upvote_status: null,
       description: desc1,
       background_image:
@@ -90,8 +87,83 @@ const PostStory1 = () => {
     Promise.all(post_arr).then((res) => console.log("posted successfully"));
   };
 
-  const [title2, seTitle2] = React.useState("");
-  const [imgLink, setImgLink] = React.useState("");
+  // Posting image...
+
+  const imageRef = React.useRef();
+  const [imageLink2, setImageLink2] = React.useState("");
+  const [title2, setTitle2] = React.useState("");
+
+  const postRequest = (imgValue) => {
+    console.log(title2, imgValue, subreddit);
+
+    var post_id = uuid();
+
+    var subredditName = "sports";
+
+    if (subreddit !== "") {
+      subredditName = subreddit.split("/")[1];
+    }
+
+    var payload1 = {
+      id: post_id,
+      title: title2,
+      upvotes: 0,
+      image_src: imgValue,
+      upvote_status: null,
+      background_image:
+        "https://images.unsplash.com/photo-1593642532400-2682810df593?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
+      subreddit: subredditName,
+      username: "udit22022000",
+      comment_count: 0,
+      comments: [],
+    };
+
+    var payload2 = {
+      id: uuid(),
+      subreddit: subredditName,
+      post_id: post_id,
+    };
+
+    var post_arr = [
+      axios.post(
+        `https://reddit-mock-server.herokuapp.com/${subredditName}`,
+        payload1
+      ),
+      axios.post("https://reddit-mock-server.herokuapp.com/hot", payload2),
+    ];
+
+    Promise.all(post_arr).then((res) => console.log("posted successfully"));
+  };
+
+  const handleSubmitImage1 = (e) => {
+    e.preventDefault();
+    postRequest(imageLink2);
+  };
+
+  const handleSubmitImage2 = async (e) => {
+    e.preventDefault();
+    console.log(imageRef.current.files[0]);
+    try {
+      const formdata = new FormData();
+      formdata.append(
+        "image",
+        imageRef.current.files[0],
+        imageRef.current.files[0].name
+      );
+
+      const res = await fetch("https://api.imgur.com/3/image", {
+        method: "POST",
+        headers: {
+          Authorization: "Client-ID 4433e1304c60685",
+        },
+        body: formdata,
+      }).then((res) => res.json());
+
+      postRequest(res.data.link);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className={styles.mainContainer}>
@@ -142,7 +214,40 @@ const PostStory1 = () => {
               <button onClick={handlePostSubmit1}>POST</button>
             </div>
             <div className={imgVidStyle}>
-              <ImgVidForm />
+              <div>
+                {window.location.href.includes("localhost") ? (
+                  <form
+                    onSubmit={handleSubmitImage1}
+                    className={styles.inputArea2}
+                  >
+                    <input
+                      value={title2}
+                      onChange={(e) => setTitle2(e.target.value)}
+                      placeholder="Enter Title"
+                    />
+                    <input
+                      placeholder="Enter Image Link"
+                      value={imageLink2}
+                      onChange={(e) => setImageLink2(e.target.value)}
+                    />
+                    <input type="submit" />
+                  </form>
+                ) : (
+                  <form
+                    onSubmit={handleSubmitImage2}
+                    className={styles.inputArea2}
+                  >
+                    <input
+                      value={title2}
+                      onChange={(e) => setTitle2(e.target.value)}
+                      placeholder="Enter Title"
+                    />
+                    <span>Choose an Image :</span>
+                    <input ref={imageRef} type="file" />
+                    <input type="submit" />
+                  </form>
+                )}
+              </div>
             </div>
           </div>
         </div>
